c8299fa8bb1fff3639de1e874bece05e83a1d30b
Lucas Cavenaghi <lucas.ag.cv@gmail.com>
2022-10-21 10:21:55 -0300

    adiciona logout

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -10,78 +10,95 @@ import {
   SideNav,
   SideNavItems,
   HeaderSideNavItems,
+  HeaderGlobalBar,
+  HeaderGlobalAction
 } from '@carbon/react';
+import { Logout } from '@carbon/react/icons';
 import { Link } from 'react-router-dom';
 
-const MainHeader = () => (
-  <HeaderContainer
-    render={({ isSideNavExpanded, onClickSideNavExpand }) => (
-      <Header aria-label="Games List">
-        <SkipToContent />
-        <HeaderMenuButton
-          aria-label="Open menu"
-          onClick={onClickSideNavExpand}
-          isActive={isSideNavExpanded}
-        />
-        <HeaderName element={Link} to="/" prefix="">
-          Games List
-        </HeaderName>
-        <HeaderNavigation aria-label="Games List">
-          <HeaderMenuItem element={Link} to="/acessos">
-            Meus Acessos
-          </HeaderMenuItem>
-          <HeaderMenuItem element={Link} to="/noticias">
-            Notícias
-          </HeaderMenuItem>
-          <HeaderMenuItem element={Link} to="/jogos">
-            Jogos
-          </HeaderMenuItem>
-          <HeaderMenuItem element={Link} to="/tags">
-            Tags
-          </HeaderMenuItem>
-          <HeaderMenuItem element={Link} to="/plataformas">
-            Plataformas
-          </HeaderMenuItem>
-          <HeaderMenuItem element={Link} to="/lojas">
-            Lojas
-          </HeaderMenuItem>
-          <HeaderMenuItem element={Link} to="/desenvolvedoras">
-            Desenvolvedoras
-          </HeaderMenuItem>
-        </HeaderNavigation>
-        <SideNav
-          aria-label="Side navigation"
-          expanded={isSideNavExpanded}
-          isPersistent={false}>
-          <SideNavItems>
-            <HeaderSideNavItems>
-              <HeaderMenuItem element={Link} to="/acessos">
+const MainHeader = () => {
+
+  const logout = (e) => {
+    console.log("Tô deslogando, valeu")
+    localStorage.removeItem("token");
+    window.location.href = '#/login'
+  }
+
+  return (
+    <HeaderContainer
+      render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+        <Header aria-label="Games List">
+          <SkipToContent />
+          <HeaderMenuButton
+            aria-label="Open menu"
+            onClick={onClickSideNavExpand}
+            isActive={isSideNavExpanded}
+          />
+          <HeaderName element={Link} to="/" prefix="">
+            Games List
+          </HeaderName>
+          <HeaderNavigation aria-label="Games List">
+            <HeaderMenuItem element={Link} to="/acessos">
               Meus Acessos
-              </HeaderMenuItem>
-              <HeaderMenuItem element={Link} to="/notícias">
-                Notícias
-              </HeaderMenuItem>
-              <HeaderMenuItem element={Link} to="/jogos">
-                Jogos
-              </HeaderMenuItem>
-              <HeaderMenuItem element={Link} to="/tags">
-                Tags
-              </HeaderMenuItem>
-              <HeaderMenuItem element={Link} to="/plataformas">
-                Plataformas
-              </HeaderMenuItem>
-              <HeaderMenuItem element={Link} to="/lojas">
-                Lojas
-              </HeaderMenuItem>
-              <HeaderMenuItem element={Link} to="/desenvolvedoras">
-                Desenvolvedoras
-              </HeaderMenuItem>
-            </HeaderSideNavItems>
-          </SideNavItems>
-        </SideNav>
-      </Header>
-    )}
-  />
-);
+            </HeaderMenuItem>
+            <HeaderMenuItem element={Link} to="/noticias">
+              Notícias
+            </HeaderMenuItem>
+            <HeaderMenuItem element={Link} to="/jogos">
+              Jogos
+            </HeaderMenuItem>
+            <HeaderMenuItem element={Link} to="/tags">
+              Tags
+            </HeaderMenuItem>
+            <HeaderMenuItem element={Link} to="/plataformas">
+              Plataformas
+            </HeaderMenuItem>
+            <HeaderMenuItem element={Link} to="/lojas">
+              Lojas
+            </HeaderMenuItem>
+            <HeaderMenuItem element={Link} to="/desenvolvedoras">
+              Desenvolvedoras
+            </HeaderMenuItem>
+          </HeaderNavigation>
+          <SideNav
+            aria-label="Side navigation"
+            expanded={isSideNavExpanded}
+            isPersistent={false}>
+            <SideNavItems>
+              <HeaderSideNavItems>
+                <HeaderMenuItem element={Link} to="/acessos">
+                  Meus Acessos
+                </HeaderMenuItem>
+                <HeaderMenuItem element={Link} to="/notícias">
+                  Notícias
+                </HeaderMenuItem>
+                <HeaderMenuItem element={Link} to="/jogos">
+                  Jogos
+                </HeaderMenuItem>
+                <HeaderMenuItem element={Link} to="/tags">
+                  Tags
+                </HeaderMenuItem>
+                <HeaderMenuItem element={Link} to="/plataformas">
+                  Plataformas
+                </HeaderMenuItem>
+                <HeaderMenuItem element={Link} to="/lojas">
+                  Lojas
+                </HeaderMenuItem>
+                <HeaderMenuItem element={Link} to="/desenvolvedoras">
+                  Desenvolvedoras
+                </HeaderMenuItem>
+              </HeaderSideNavItems>
+            </SideNavItems>
+          </SideNav>
+          <HeaderGlobalBar>
+            <HeaderGlobalAction onClick={logout} aria-label="Logout">
+              <Logout size={20} />
+            </HeaderGlobalAction>
+          </HeaderGlobalBar>
+        </Header>
+      )}
+    />
+  );
+};
 
 export default MainHeader;
